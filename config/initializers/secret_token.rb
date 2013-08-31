faa9ceff429b7081253a19ad0de12bb03debd71f
Pinak <pinak@complitech.net>
2013-08-31 13:53:27 +0530

    heroku mongodb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4Mongo::Application.config.secret_key_base = 'b9ad12d5aa9653259be9b3a31e2e8a80b42d0cf9703735cde1d2847d27443f1c1ac6859bba5c50da3846f7f78c169ccc037e7acd62c52eafe867bc5eb53f51da'
